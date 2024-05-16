59b105cecc6bbc9fe5be266b89a6a94716890af6
Obi1 <obi@vnight-studios.xyz>
2024-05-16 22:48:15 +0200

    Add files via upload

---
+++ NoMove/Main.cs
@@ -0,0 +1,32 @@
+﻿using MelonLoader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NoMove
+{
+    internal class Main
+    {
+        public static void Start()
+        {
+            MelonCoroutines.Start(WaitForCoHTML());
+        }
+
+
+
+
+        public static IEnumerator WaitForCoHTML()
+        {
+            while (GameObject.Find("/Cohtml") == null)
+            {
+                yield return null;
+            }
+            Console.WriteLine("Got CoHTML!");
+            UI.setUi();
+        }
+    }
+}
